93af3b33779ed0a2f08ceef04b26458d70a5af5e
ValeriCuk <pakutasieva@gmail.com>
2025-02-07 11:29:30 +0200

    Add func getOnlineUsers, logout

--- src/main/java/academy/prog/Main.java
+++ src/main/java/academy/prog/Main.java
@@ -1,6 +1,13 @@
 package academy.prog;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,11 +22,18 @@ public class Main {
 			Thread th = new Thread(new GetThread(login));
 			th.setDaemon(true);
 			th.start();
-
+			System.out.println("To get a list of online users type: \n#online-users\n");
             System.out.println("Enter your message: ");
 			while (true) {
 				String text = scanner.nextLine();
-				if (text.isEmpty()) break;
+				if (text.isEmpty()) {
+					int res = logout(login);
+					System.out.println(res == 200 ? "You logged out successfully" : "You are not logged out");
+					break;
+				}else if (text.contains("#online-users")){
+					System.out.println("Get a list of users online");
+					System.out.println(getOnlineUsers());
+				}
 				String to = getMessageRecipient(text);
 				Message m = new Message(login, to, text);
 				int res = m.send(Utils.getURL() + "/add");
@@ -40,5 +54,36 @@ public class Main {
 		Pattern pattern = Pattern.compile("@(\\S+)");
 		Matcher matcher = pattern.matcher(message);
 		return matcher.find() ? matcher.group(1) : null;
+	}
+
+	private static int logout(String user) throws IOException{
+		URL obj = new URL(Utils.getURL() + "/logout?username=" + user);
+		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+		conn.setRequestMethod("DELETE");
+		conn.setDoOutput(true);
+		return conn.getResponseCode();
+	}
+
+	private static String getOnlineUsers()  throws IOException{
+		URL obj = new URL(Utils.getURL() + "/users-online");
+		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setDoOutput(true);
+		if (conn.getResponseCode() == 200) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String inputLine;
+			StringBuilder sb = new StringBuilder();
+			while ((inputLine = in.readLine()) != null) {
+				sb.append(inputLine);
+				sb.append(System.lineSeparator());
+			}
+			in.close();
+
+			return sb.toString();
+
+		}else {
+			System.out.println("Error: " + conn.getResponseCode());
+			return "\nNo one online\n";
+		}
 	}
 }
